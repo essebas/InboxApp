306d5c8dcb702c63c73f33e64295da3daea696d5
sebas.camargo <sebas.camargo81@gmail.com>
2021-10-13 23:52:19 -0500

    Define remote data source interface and his implementation class to posts objects. Implementation class use a instance of PostServices to call posts services

---
+++ app/src/main/java/com/zebas2/inboxapp/data/repository/datasource/PostRemoteDataSource.kt
@@ -0,0 +1,10 @@
+package com.zebas2.inboxapp.data.repository.datasource
+
+import com.zebas2.inboxapp.data.model.Post
+import retrofit2.Response
+
+interface PostRemoteDataSource {
+
+    suspend fun getPosts(): Response<List<Post>>
+
+}

3d020d8a8da50713fa43b9af1b965c04487eebfe
sebas.camargo <sebas.camargo81@gmail.com>
2021-10-14 10:55:42 -0500

    Create method to get Messages from viewModel and observe Mutable List of Post model to get changes

--- app/src/main/java/com/zebas2/inboxapp/MessagesFragment.kt
+++ app/src/main/java/com/zebas2/inboxapp/MessagesFragment.kt
@@ -1,12 +1,21 @@
 package com.zebas2.inboxapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.zebas2.inboxapp.databinding.FragmentMessagesBinding
+import com.zebas2.inboxapp.presentation.MainActivity
+import com.zebas2.inboxapp.presentation.viewmodel.MessagesViewModel
+
+private const val TAG = "MessagesFragment"
 
 class MessagesFragment : Fragment() {
+
+    private lateinit var viewModel: MessagesViewModel
+    private lateinit var binding: FragmentMessagesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +23,23 @@ class MessagesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_messages, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMessagesBinding.bind(view)
+        viewModel = (activity as MainActivity).viewModel
+
+        viewMessagesList()
+    }
+
+    fun viewMessagesList() {
+        viewModel.getMessages()
+        viewModel.messages.observe(viewLifecycleOwner, { response ->
+            Log.d(TAG, "viewMessagesList: ${response.toString()}")
+            when (response) {
+            }
+        })
     }
 
 }

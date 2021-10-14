d6d31387b6e7b70005b3ef2b4c1b37d035d61edb
sebas.camargo <sebas.camargo81@gmail.com>
2021-10-14 15:45:02 -0500

    Create dependencies injection to userViewModel in FactoryModule.kt

--- app/src/main/java/com/zebas2/inboxapp/di/FactoryModule.kt
+++ app/src/main/java/com/zebas2/inboxapp/di/FactoryModule.kt
@@ -2,7 +2,9 @@ package com.zebas2.inboxapp.di
 
 import android.app.Application
 import com.zebas2.inboxapp.domain.usecase.GetMessagesUseCase
+import com.zebas2.inboxapp.domain.usecase.GetUserDetailUseCase
 import com.zebas2.inboxapp.presentation.viewmodel.MessagesViewModelFactory
+import com.zebas2.inboxapp.presentation.viewmodel.UserViewModelFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,5 +22,14 @@ class FactoryModule {
         getMessagesUseCase: GetMessagesUseCase
     ): MessagesViewModelFactory {
         return MessagesViewModelFactory(application, getMessagesUseCase)
+    }
+
+    @Singleton
+    @Provides
+    fun provideUserViewModelFactory(
+        application: Application,
+        getUserDetailUseCase: GetUserDetailUseCase
+    ): UserViewModelFactory {
+        return UserViewModelFactory(application, getUserDetailUseCase)
     }
 }

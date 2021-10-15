3553325d8e07ead73eabae76cdf88da9db5ba5d6
sebas.camargo <sebas.camargo81@gmail.com>
2021-10-15 01:26:03 -0500

    Add entity annotation to room persistence can create table. Define primary key with post id

--- app/src/main/java/com/zebas2/inboxapp/data/model/Post.kt
+++ app/src/main/java/com/zebas2/inboxapp/data/model/Post.kt
@@ -1,16 +1,26 @@
 package com.zebas2.inboxapp.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
+@Entity(
+    tableName = "posts"
+)
 data class Post(
-    @SerializedName("userId")
-    val userId: Int,
+
+    @PrimaryKey
     @SerializedName("id")
     val id: Int,
+
+    @SerializedName("userId")
+    val userId: Int,
     @SerializedName("title")
     val title: String,
     @SerializedName("body")
     val body: String,
+    @SerializedName("isRead")
     var isRead: Boolean = false
 ) : Serializable
